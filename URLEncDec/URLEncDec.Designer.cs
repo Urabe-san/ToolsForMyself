e88f2c0d34472658144e34848719c262ca80edd5
your Father <c.jet@outlook.com>
2024-08-23 07:15:01 +0900

    Add files via upload

---
+++ URLEncDec/URLEncDec.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace URLEncDec
+{
+    partial class URLEncDec
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textboxExpression = new System.Windows.Forms.TextBox();
+            this.buttonEncode = new System.Windows.Forms.Button();
+            this.buttonDecode = new System.Windows.Forms.Button();
+            this.textboxResult = new System.Windows.Forms.TextBox();
+            this.buttonQuit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textboxExpression
+            // 
+            this.textboxExpression.Location = new System.Drawing.Point(12, 12);
+            this.textboxExpression.Name = "textboxExpression";
+            this.textboxExpression.Size = new System.Drawing.Size(487, 22);
+            this.textboxExpression.TabIndex = 0;
+            // 
+            // buttonEncode
+            // 
+            this.buttonEncode.Location = new System.Drawing.Point(343, 40);
+            this.buttonEncode.Name = "buttonEncode";
+            this.buttonEncode.Size = new System.Drawing.Size(75, 23);
+            this.buttonEncode.TabIndex = 1;
+            this.buttonEncode.Text = "Encode";
+            this.buttonEncode.UseVisualStyleBackColor = true;
+            this.buttonEncode.Click += new System.EventHandler(this.buttonEncode_Click);
+            // 
+            // buttonDecode
+            // 
+            this.buttonDecode.Location = new System.Drawing.Point(424, 40);
+            this.buttonDecode.Name = "buttonDecode";
+            this.buttonDecode.Size = new System.Drawing.Size(75, 23);
+            this.buttonDecode.TabIndex = 2;
+            this.buttonDecode.Text = "Decode";
+            this.buttonDecode.UseVisualStyleBackColor = true;
+            this.buttonDecode.Click += new System.EventHandler(this.buttonDecode_Click);
+            // 
+            // textboxResult
+            // 
+            this.textboxResult.Location = new System.Drawing.Point(12, 69);
+            this.textboxResult.Name = "textboxResult";
+            this.textboxResult.Size = new System.Drawing.Size(487, 22);
+            this.textboxResult.TabIndex = 3;
+            // 
+            // buttonQuit
+            // 
+            this.buttonQuit.Location = new System.Drawing.Point(424, 97);
+            this.buttonQuit.Name = "buttonQuit";
+            this.buttonQuit.Size = new System.Drawing.Size(75, 23);
+            this.buttonQuit.TabIndex = 4;
+            this.buttonQuit.Text = "Quit";
+            this.buttonQuit.UseVisualStyleBackColor = true;
+            this.buttonQuit.Click += new System.EventHandler(this.buttonQuit_Click);
+            // 
+            // URLEncDec
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 133);
+            this.Controls.Add(this.buttonQuit);
+            this.Controls.Add(this.textboxResult);
+            this.Controls.Add(this.buttonDecode);
+            this.Controls.Add(this.buttonEncode);
+            this.Controls.Add(this.textboxExpression);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "URLEncDec";
+            this.Text = "URL Encode Decode";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textboxExpression;
+        private System.Windows.Forms.Button buttonEncode;
+        private System.Windows.Forms.Button buttonDecode;
+        private System.Windows.Forms.TextBox textboxResult;
+        private System.Windows.Forms.Button buttonQuit;
+    }
+}
+
---
+++ URLEncDec/URLEncDec.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace URLEncDec
+{
+    public partial class URLEncDec : Form
+    {
+        public URLEncDec()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonQuit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonEncode_Click(object sender, EventArgs e)
+        {
+            if (0 < textboxExpression.Text.Length)
+            {
+                textboxResult.Text = System.Web.HttpUtility.UrlEncode(textboxExpression.Text);
+            }
+        }
+
+        private void buttonDecode_Click(object sender, EventArgs e)
+        {
+            if (0 < textboxExpression.Text.Length)
+            {
+                textboxResult.Text = System.Web.HttpUtility.UrlDecode(textboxExpression.Text);
+            }
+        }
+    }
+}
