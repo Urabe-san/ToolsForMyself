00f6012df9fdbbeacace989ec586391b0aa923a9
your Father <c.jet@outlook.com>
2024-11-23 10:08:59 +0900

    UrlSomething modified

--- UrlSomething/UrlSomething.Designer.cs
+++ UrlSomething/UrlSomething.Designer.cs
@@ -33,21 +33,26 @@
             this.buttonDecode = new System.Windows.Forms.Button();
             this.textboxResult = new System.Windows.Forms.TextBox();
             this.buttonQuit = new System.Windows.Forms.Button();
+            this.buttonPaste = new System.Windows.Forms.Button();
+            this.buttonPastePlus = new System.Windows.Forms.Button();
+            this.buttonCopyToClipboard = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textboxOrigin
             // 
-            this.textboxOrigin.Location = new System.Drawing.Point(12, 12);
+            this.textboxOrigin.Location = new System.Drawing.Point(16, 15);
+            this.textboxOrigin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textboxOrigin.Multiline = true;
             this.textboxOrigin.Name = "textboxOrigin";
-            this.textboxOrigin.Size = new System.Drawing.Size(469, 68);
+            this.textboxOrigin.Size = new System.Drawing.Size(624, 84);
             this.textboxOrigin.TabIndex = 1;
             // 
             // buttonClear
             // 
-            this.buttonClear.Location = new System.Drawing.Point(487, 12);
+            this.buttonClear.Location = new System.Drawing.Point(649, 15);
+            this.buttonClear.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonClear.Name = "buttonClear";
-            this.buttonClear.Size = new System.Drawing.Size(52, 23);
+            this.buttonClear.Size = new System.Drawing.Size(69, 29);
             this.buttonClear.TabIndex = 2;
             this.buttonClear.Text = "Clear";
             this.buttonClear.UseVisualStyleBackColor = true;
@@ -55,43 +60,80 @@
             // 
             // buttonDecode
             // 
-            this.buttonDecode.Location = new System.Drawing.Point(406, 86);
+            this.buttonDecode.Location = new System.Drawing.Point(541, 108);
+            this.buttonDecode.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonDecode.Name = "buttonDecode";
-            this.buttonDecode.Size = new System.Drawing.Size(75, 23);
-            this.buttonDecode.TabIndex = 3;
+            this.buttonDecode.Size = new System.Drawing.Size(100, 29);
+            this.buttonDecode.TabIndex = 5;
             this.buttonDecode.Text = "DECODE";
             this.buttonDecode.UseVisualStyleBackColor = true;
             this.buttonDecode.Click += new System.EventHandler(this.buttonDecode_Click);
             // 
             // textboxResult
             // 
-            this.textboxResult.Location = new System.Drawing.Point(12, 115);
+            this.textboxResult.Location = new System.Drawing.Point(16, 144);
+            this.textboxResult.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textboxResult.Multiline = true;
             this.textboxResult.Name = "textboxResult";
-            this.textboxResult.Size = new System.Drawing.Size(469, 68);
-            this.textboxResult.TabIndex = 4;
+            this.textboxResult.Size = new System.Drawing.Size(624, 84);
+            this.textboxResult.TabIndex = 6;
             // 
             // buttonQuit
             // 
-            this.buttonQuit.Location = new System.Drawing.Point(406, 189);
+            this.buttonQuit.Location = new System.Drawing.Point(541, 236);
+            this.buttonQuit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonQuit.Name = "buttonQuit";
-            this.buttonQuit.Size = new System.Drawing.Size(75, 23);
+            this.buttonQuit.Size = new System.Drawing.Size(100, 29);
             this.buttonQuit.TabIndex = 0;
             this.buttonQuit.Text = "Quit";
             this.buttonQuit.UseVisualStyleBackColor = true;
             this.buttonQuit.Click += new System.EventHandler(this.buttonQuit_Click);
             // 
+            // buttonPaste
+            // 
+            this.buttonPaste.Location = new System.Drawing.Point(649, 51);
+            this.buttonPaste.Name = "buttonPaste";
+            this.buttonPaste.Size = new System.Drawing.Size(24, 24);
+            this.buttonPaste.TabIndex = 3;
+            this.buttonPaste.Text = "P";
+            this.buttonPaste.UseVisualStyleBackColor = true;
+            this.buttonPaste.Click += new System.EventHandler(this.buttonPaste_Click);
+            // 
+            // buttonPastePlus
+            // 
+            this.buttonPastePlus.Location = new System.Drawing.Point(679, 51);
+            this.buttonPastePlus.Name = "buttonPastePlus";
+            this.buttonPastePlus.Size = new System.Drawing.Size(39, 24);
+            this.buttonPastePlus.TabIndex = 4;
+            this.buttonPastePlus.Text = "P+";
+            this.buttonPastePlus.UseVisualStyleBackColor = true;
+            this.buttonPastePlus.Click += new System.EventHandler(this.buttonPastePlus_Click);
+            // 
+            // buttonCopyToClipboard
+            // 
+            this.buttonCopyToClipboard.Location = new System.Drawing.Point(649, 144);
+            this.buttonCopyToClipboard.Name = "buttonCopyToClipboard";
+            this.buttonCopyToClipboard.Size = new System.Drawing.Size(24, 24);
+            this.buttonCopyToClipboard.TabIndex = 7;
+            this.buttonCopyToClipboard.Text = "C";
+            this.buttonCopyToClipboard.UseVisualStyleBackColor = true;
+            this.buttonCopyToClipboard.Click += new System.EventHandler(this.buttonCopyToClipboard_Click);
+            // 
             // UrlSomething
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(548, 221);
+            this.ClientSize = new System.Drawing.Size(731, 276);
+            this.Controls.Add(this.buttonCopyToClipboard);
+            this.Controls.Add(this.buttonPastePlus);
+            this.Controls.Add(this.buttonPaste);
             this.Controls.Add(this.buttonQuit);
             this.Controls.Add(this.textboxResult);
             this.Controls.Add(this.buttonDecode);
             this.Controls.Add(this.buttonClear);
             this.Controls.Add(this.textboxOrigin);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "UrlSomething";
@@ -111,6 +153,9 @@
         private System.Windows.Forms.Button buttonDecode;
         private System.Windows.Forms.TextBox textboxResult;
         private System.Windows.Forms.Button buttonQuit;
+        private System.Windows.Forms.Button buttonPaste;
+        private System.Windows.Forms.Button buttonPastePlus;
+        private System.Windows.Forms.Button buttonCopyToClipboard;
     }
 }
 
--- UrlSomething/UrlSomething.cs
+++ UrlSomething/UrlSomething.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 using System.Web;
+using System.Xml.Linq;
+using System.Linq.Expressions;
 
 namespace UrlSomething
 {
@@ -39,7 +41,7 @@ namespace UrlSomething
 
         private void buttonClear_Click(object sender, EventArgs evt)
         {
-            textboxOrigin.Text = string.Empty;
+            ClearTextBoxes();
         }
 
         private void buttonDecode_Click(object sender, EventArgs evt)
@@ -60,6 +62,24 @@ namespace UrlSomething
             }
         }
 
+        private void buttonPaste_Click(object sender, EventArgs e)
+        {
+            PasteToOrigin();
+        }
+
+        private void buttonPastePlus_Click(object sender, EventArgs e)
+        {
+            PasteToOrigin(true);
+        }
+
+        private void buttonCopyToClipboard_Click(object sender, EventArgs e)
+        {
+            if(0<textboxResult.Text.Length)
+            {
+                CopyToClipboard(textboxResult.Text);
+            }
+        }
+
         private void buttonQuit_Click(object sender, EventArgs evt)
         {
             if (MessageBox.Show("Quit this application", this.Name, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
@@ -67,6 +87,45 @@ namespace UrlSomething
                 IsClose = true;
                 this.Close();
             }
+        }
+
+        private void WriteEventLog(string expression, string source = "")
+        {
+            string sourcevalue = string.Empty;
+
+            if(string.IsNullOrEmpty(source))
+            {
+                sourcevalue = System.AppDomain.CurrentDomain.FriendlyName;
+                if (string.IsNullOrEmpty(sourcevalue))
+                {
+                    sourcevalue = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+                }
+            }
+            else
+            {
+                sourcevalue = source;
+            }
+
+            //Create source, when source is not exists.
+            if (!System.Diagnostics.EventLog.SourceExists(sourcevalue))
+            {
+
+                System.Diagnostics.EventLog.CreateEventSource(sourcevalue, "");
+            }
+            try
+            {
+                System.Diagnostics.EventLog.WriteEntry(sourcevalue, expression, System.Diagnostics.EventLogEntryType.Error);
+            }
+            catch
+            {
+                //NOP
+            }
+        }
+
+        private void ClearTextBoxes()
+        {
+            textboxOrigin.Text = string.Empty;
+            textboxResult.Text = string.Empty;
         }
 
         private string Decode(string expression)
@@ -90,19 +149,75 @@ namespace UrlSomething
                 origin = expression.Substring(HTTP_STRING.Length);
             }
             else
-            { 
-                origin = expression; 
+            {
+                origin = expression;
             }
 
             result = HttpUtility.UrlDecode(origin);
 
-            if(IsHttps)
+            if (IsHttps)
             {
                 result = HTTPS_STRING + result;
             }
-            else if(IsHttp)
+            else if (IsHttp)
             {
                 result = HTTP_STRING + result;
+            }
+
+            return result;
+        }
+
+        private void PasteToOrigin(bool WithDecode = false)
+        {
+            //textboxOrigin.Paste(); //perhaps, this one line will accomplish my goal.
+
+            IDataObject iDataInstance = Clipboard.GetDataObject();
+
+            // most of the "else" is unnecessary, when i clear the values ​​in the text boxes first.
+            // although, this program also serves as a test.
+            if (iDataInstance.GetDataPresent(DataFormats.Text))
+            {
+                string TextData = (String)iDataInstance.GetData(DataFormats.Text);
+
+                if (!string.IsNullOrEmpty(TextData))
+                {
+                    textboxOrigin.Text = TextData;
+                    if (WithDecode)
+                    {
+                        if (TextData.Length > 0)
+                        {
+                            textboxResult.Text = this.Decode(TextData);
+                        }
+                        else
+                        {
+                            textboxResult.Text = string.Empty;
+                        }
+                    }
+                    else
+                    {
+                        textboxResult.Text = string.Empty;
+                    }
+                }
+                else
+                {
+                    ClearTextBoxes();
+                }
+            }
+        }
+
+        private bool CopyToClipboard(string Value)
+        {
+            bool result = false;
+
+            try
+            {
+                Clipboard.SetText(Value);
+                result = true;
+            }
+            catch (Exception exc)
+            {
+                result = false;
+                WriteEventLog(exc.Message);
             }
 
             return result;
