267784126ca7170064443d9bc5067c916df19691
your Father <c.jet@outlook.com>
2025-04-05 08:57:17 +0900

    Add Directory - DesktopNote

---
+++ DesktopNote/DesktopNote.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace DesktopNote
+{
+    partial class DesktopNote
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DesktopNote));
+            this.textboxNote = new System.Windows.Forms.TextBox();
+            this.notifyIconDesktopNote = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextmenustripTaskTray = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextmenustripTaskTray.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textboxNote
+            // 
+            this.textboxNote.Location = new System.Drawing.Point(0, 0);
+            this.textboxNote.Multiline = true;
+            this.textboxNote.Name = "textboxNote";
+            this.textboxNote.Size = new System.Drawing.Size(298, 193);
+            this.textboxNote.TabIndex = 0;
+            // 
+            // notifyIconDesktopNote
+            // 
+            this.notifyIconDesktopNote.ContextMenuStrip = this.contextmenustripTaskTray;
+            this.notifyIconDesktopNote.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIconDesktopNote.Icon")));
+            this.notifyIconDesktopNote.Text = "notifyIconDesktopNote";
+            this.notifyIconDesktopNote.Visible = true;
+            // 
+            // contextmenustripTaskTray
+            // 
+            this.contextmenustripTaskTray.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextmenustripTaskTray.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.contextmenustripTaskTray.Name = "contextmenustripTaskTray";
+            this.contextmenustripTaskTray.Size = new System.Drawing.Size(112, 76);
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(111, 24);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(111, 24);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(111, 24);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // DesktopNote
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 193);
+            this.ControlBox = false;
+            this.Controls.Add(this.textboxNote);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "DesktopNote";
+            this.ShowInTaskbar = false;
+            this.Text = "Desktop Note";
+            this.Load += new System.EventHandler(this.DesktopNote_Load);
+            this.Resize += new System.EventHandler(this.DesktopNote_Resize);
+            this.contextmenustripTaskTray.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textboxNote;
+        private System.Windows.Forms.NotifyIcon notifyIconDesktopNote;
+        private System.Windows.Forms.ContextMenuStrip contextmenustripTaskTray;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+    }
+}
+
---
+++ DesktopNote/DesktopNote.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DesktopNote
+{
+    public partial class DesktopNote : Form
+    {
+
+        private const string FILE_NAME = "DesktopNote.txt";
+
+        public DesktopNote()
+        {
+            InitializeComponent();
+        }
+
+        private void DesktopNote_Load(object sender, EventArgs e)
+        {
+            NoteResize();
+        }
+
+        private void DesktopNote_Resize(object sender, EventArgs e)
+        {
+            NoteResize();
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string Value = string.Empty;
+            try
+            {
+                Value = LoadFile(GetApplicationDirectoryPath() + "\\" + FILE_NAME);
+                textboxNote.Text = Value;
+            }
+            catch {
+                //nop
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFile(GetApplicationDirectoryPath() + "\\" + FILE_NAME, textboxNote.Text);
+            }
+            catch
+            {
+                //nop
+            }
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private static void SaveFile(string FileFullPath, string Value)
+        {
+            try
+            {
+                System.IO.File.WriteAllText(FileFullPath, Value, System.Text.Encoding.UTF8);
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine($"Error in Write Process: {ex.Message}");
+                throw new Exception("Error in Write Process: {ex.Message}", ex);
+            }
+        }
+        private void NoteResize()
+        {
+            textboxNote.Size = this.ClientSize;
+        }
+
+        private static string LoadFile(string FileFullPath)
+        {
+            try
+            {
+                if (System.IO.File.Exists(FileFullPath))
+                {
+                    return System.IO.File.ReadAllText(FileFullPath, System.Text.Encoding.UTF8);
+                }
+                else
+                {
+                    //return string.Empty;
+                    throw new System.Exception("LoadFile is not Exists.");
+                }
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine($"Error upon Load Process: {ex.Message}");
+                //return string.Empty;
+                throw new Exception("Error upon Load Process: {ex.Message}",ex);
+            }
+        }
+
+        public static string GetApplicationDirectoryPath()
+        {
+            string assemblyPath = Assembly.GetExecutingAssembly().Location;
+            return StripPath(Path.GetDirectoryName(assemblyPath));
+        }
+
+        private static string StripPath(string expression)
+        {
+            int ThisLength = expression.Trim().Length;
+
+            if (0 == ThisLength)
+            {
+                return expression.Trim();
+            }
+
+            if (expression.Trim().Substring(ThisLength - 1, 1).Equals("\\"))
+            {
+                return expression.Trim().Substring(0, ThisLength - 1);
+            }
+            else
+            {
+                return expression.Trim();
+            }
+        }
+    }
+
+}
